e95e9eab9f51987df49e29f58e35288f8cb0d5fc
P-Romilo <romilo.o@yandex.ru>
2023-01-04 00:31:42 +0300

    Приложения Счетчик для practicum.yandex

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CountLabel: UILabel!
+    var Cout: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ButtonPlus(_ sender: Any) {
+        Cout =  Cout + 1
+        CountLabel.text = "Значение счётчика:\(Cout)"
+    }
+    
+    @IBAction func ButtonMinus(_ sender: Any) {
+        Cout = Cout - 1
+        CountLabel.text = "Значение счётчика:\(Cout)"
+    }
 }
 
